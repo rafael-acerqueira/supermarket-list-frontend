14ea888adf8c7fcce28779ccf1a9f95bec096101
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-03-09 19:27:14 -0300

    Show icon whether the shopping list is done or not

--- src/components/ShoppingList/List/List.js
+++ src/components/ShoppingList/List/List.js
@@ -21,22 +21,40 @@ const ShoppingList = (props) => (
     <div className='center-content wrapper-content'>
       <Row gutter={16}>
         {
-          props.shoppingLists.map( shoppingList => (
-            <Col span={8} key={shoppingList._id} className='supermarket-item'>
-              <Card title={moment.utc(shoppingList.date).format('DD/MM/YYYY')}>
-                <Tooltip title="Editar">
-                  <Link to={`/shopping-lists/${shoppingList._id}/edit`}>
-                    <Icon type="edit" />
-                  </Link>
-                </Tooltip>
-                <Tooltip title="Apagar">
-                  <a onClick={() => props.handleRemove(shoppingList._id)}>
-                    <Icon type="delete" />
-                  </a>
-                </Tooltip>
-              </Card>
-            </Col>
-          ))
+          props.shoppingLists.map( shoppingList => {
+            const icon =
+                shoppingList.done
+                  ? <Tooltip title="Finalizado"><Icon type="check" /></Tooltip>
+                  : <Tooltip title="Não Finalizado"><Icon type="stop" /></Tooltip>
+            return (
+              <Col span={8} key={shoppingList._id} className='supermarket-item'>
+                <Card
+                  title={moment.utc(shoppingList.date).format('DD/MM/YYYY')}
+                  extra={icon}
+                >
+                  <Tooltip title="Editar">
+                    <Link to={`/shopping-lists/${shoppingList._id}/edit`}>
+                      <Icon type="edit" />
+                    </Link>
+                  </Tooltip>
+                  <Tooltip title="Apagar">
+                    <a onClick={() => props.handleRemove(shoppingList._id)}>
+                      <Icon type="delete" />
+                    </a>
+                  </Tooltip>
+                  {
+                    shoppingList.items &&
+                    <Tooltip title="Ver itens">
+                    <Link to={`/shopping-lists/${shoppingList._id}/items`}>
+                        <Icon type="eye" />
+                    </Link>
+                  </Tooltip>
+                  }
+                </Card>
+              </Col>
+            )
+          }
+        )
         }
       </Row>
     </div>
